3fe38f1e7f03f2cbfae1bac53c70b2b1b64269b0
gimgyuwon <gimgyuwon2@gmail.com>
2025-11-20 16:54:10 +0900

    build: Fix file name case sensitivity

--- src/pages/Err401.tsx
+++ src/pages/Err401.tsx
@@ -1,11 +1,11 @@
 import Lottie from "lottie-react";
-import information from "@assets/information.json";
+import bubble from "@assets/speechbubble.json";
 
 const Err401 = () => {
   return (
     <div className="flex flex-col flex-1 items-center justify-center text-center mx-auto w-[calc(100%-40px)] gap-y-[20px]">
       <Lottie
-        animationData={information}
+        animationData={bubble}
         style={{ width: "150px", height: "150px" }}
       />
       <div className="title font-bold">

0dc18402a2cce8ddf627b20992f32097a1af643c
gimgyuwon <gimgyuwon2@gmail.com>
2026-01-04 10:49:21 +0900

    feat: Frontend test commit

--- src/pages/My.tsx
+++ src/pages/My.tsx
@@ -2,7 +2,7 @@ const My = () => {
   return (
     <div className="flex flex-1 flex-col gap-y-5 items-center justify-center">
       <img src="/icon/error.svg" alt="errorIcon" width="60px" />
-      <div className="title">준비중</div>
+      <div className="title">프론트엔드 테스트</div>
     </div>
   );
 };

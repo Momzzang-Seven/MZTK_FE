25cfad0b63c6b399fdb519cfc5b59e99b801bce0
gimgyuwon <gimgyuwon2@gmail.com>
2026-02-02 23:26:27 +0900

    refactor: Refactor Onboarding page

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,2 +1,3 @@
 export * from "./CommonButton";
 export * from "./CommonModal";
+export * from "./FullScreenPage";
--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -2,6 +2,7 @@ export * from "./Footer";
 export * from "./Header";
 export * from "./Layout";
 export * from "./SimpleHeader";
+export * from "./FullScreenPage";
 
 // ADMIN
 export * from "./AdminHeader";

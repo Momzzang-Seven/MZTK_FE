bb370c1fa07f1ddf17ba755f61de9a26a2879fc4
seoro <sho0814@hanyang.ac.kr>
2026-02-05 20:24:41 +0900

    chore: changed the share url

--- src/components/community/postActions/SharePost.tsx
+++ src/components/community/postActions/SharePost.tsx
@@ -6,28 +6,21 @@ interface SharePostProps {
 }
 
 const SharePost = ({ type, postId }: SharePostProps) => {
+  const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+  const COMMUNITY_BASE = `${BASE_URL}/community`;
+
+  const basePathByType: Record<PostType, string> = {
+    free: `${COMMUNITY_BASE}/free/`,
+    question: `${COMMUNITY_BASE}/question/`,
+  };
+
+  const url = basePathByType[type] + postId;
+
   const handleShareClick = () => {
     if (!navigator.share) {
       console.log("Web Share API를 지원하지 않는 환경입니다.");
       return;
     }
-
-    const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-<<<<<<< HEAD
-    const COMMUNITY_BASE = `${BASE_URL}/community`;
-
-    const basePathByType: Record<PostType, string> = {
-      free: `${COMMUNITY_BASE}/free/`,
-      question: `${COMMUNITY_BASE}/question/`,
-=======
-
-    const basePathByType: Record<PostType, string> = {
-      free: `${BASE_URL}/community/free/`,
-      question: `${BASE_URL}/community/question/`,
->>>>>>> 660367a5750716ede3c95c3ed6e70cb26dc1fd66
-    };
-
-    const url = basePathByType[type] + postId;
 
     navigator
       .share({
